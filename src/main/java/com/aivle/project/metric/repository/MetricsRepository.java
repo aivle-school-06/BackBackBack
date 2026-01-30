fa7cc97d47054f8e8f75a6918bb62b8e3dbd8700
CYPARK <chanyoung990704@naver.com>
2026-01-30 14:47:04 +0900

    feat(report): add predicted metric import service

--- src/main/java/com/aivle/project/metric/repository/MetricsRepository.java
+++ src/main/java/com/aivle/project/metric/repository/MetricsRepository.java
@@ -1,6 +1,8 @@
 package com.aivle.project.metric.repository;
 
 import com.aivle.project.metric.entity.MetricsEntity;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,4 +12,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MetricsRepository extends JpaRepository<MetricsEntity, Long> {
 
 	Optional<MetricsEntity> findByMetricCode(String metricCode);
+
+	List<MetricsEntity> findAllByMetricNameEnIn(Collection<String> metricNameEns);
 }
