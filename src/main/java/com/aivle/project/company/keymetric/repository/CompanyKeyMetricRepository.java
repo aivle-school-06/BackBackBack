1b4789374eafa84a1e8b9af0dc214318f0cd4cc0
CYPARK <chanyoung990704@naver.com>
2026-02-11 21:33:33 +0900

    perf(db): add @EntityGraph to CompanyKeyMetricRepository to solve N+1 problems

--- src/main/java/com/aivle/project/company/keymetric/repository/CompanyKeyMetricRepository.java
+++ src/main/java/com/aivle/project/company/keymetric/repository/CompanyKeyMetricRepository.java
@@ -3,6 +3,7 @@ package com.aivle.project.company.keymetric.repository;
 import com.aivle.project.company.keymetric.entity.CompanyKeyMetricEntity;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -14,7 +15,9 @@ public interface CompanyKeyMetricRepository extends JpaRepository<CompanyKeyMetr
 
 	Optional<CompanyKeyMetricEntity> findByCompanyIdAndQuarter_QuarterKey(Long companyId, int quarterKey);
 
+	@EntityGraph(attributePaths = {"company", "quarter"})
 	List<CompanyKeyMetricEntity> findByCompanyIdIn(List<Long> companyIds);
 
+	@EntityGraph(attributePaths = {"company", "quarter"})
 	List<CompanyKeyMetricEntity> findByCompanyIdInAndQuarter_QuarterKey(List<Long> companyIds, int quarterKey);
 }
